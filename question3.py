6fce0acf72186dd3cdca4d1cf4572db1b4bf0c90
Ralph Joshua <115396399+everyonehatesralph@users.noreply.github.com>
2023-10-02 06:42:41 -0700

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,9 @@
+rvalue = eval(input("Please input the value of r: "))
+avalue = eval(input("Please input the value of a: "))
+bvalue = eval(input("Please input the value of b: "))
+cvalue = eval(input("Please input the value of c: "))
+dvalue = eval(input("Please input the value of d: "))
+
+result = (4 / 3) * (rvalue + 34) - 9 * ((avalue + bvalue) * cvalue) + 3 + dvalue * (2 + avalue) / (avalue + bvalue + dvalue)
+
+print(result)
