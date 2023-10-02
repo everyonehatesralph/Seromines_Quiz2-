3bed66ecccc1e61d23633ea552f96e7cc6f651c6
Ralph Joshua <115396399+everyonehatesralph@users.noreply.github.com>
2023-10-02 06:44:01 -0700

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,7 @@
+num1 = 25
+num2 = 4
+
+result = num1 / num2
+
+print("The result before i translated into a integer is", result,"and this is the result when i translated it into a integer", int(result)) 
+
